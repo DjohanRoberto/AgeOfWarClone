db6418b49798019cf0ecee473a000c5086c057cd
tyrant <32886902+TyrantTF2@users.noreply.github.com>
2024-03-22 15:49:54 +0700

    Added tick damage with enemy type detection

--- AgeOfWar/Assets/Scripts/Troop.cs
+++ AgeOfWar/Assets/Scripts/Troop.cs
@@ -11,6 +11,12 @@ public class Troop : MonoBehaviour
     [SerializeField]
     public int bufferLength = 2;
 
+    private float Timer = 0f;
+
+    private bool Colliding = false;
+
+    Rigidbody2D rb;
+
     /*[SerializeField]
     float health, maxHealth = 10f;
 
@@ -35,25 +41,54 @@ public class Troop : MonoBehaviour
         healthBar = GetComponentInChildren<FloatingHealthBar>();
     }
     */
-    // Start is called before the first frame update
-    void Start()
+
+    private void OnCollisionStay2D(Collision2D collision)
     {
-        // healthBar.UpdateHealthBar(health, maxHealth);
+        GameObject other = collision.gameObject;
+        /*if (other.gameObject.name != "Platform")
+        {
+            this.Colliding = true;
+        }
+        */
+        if (other.CompareTag("Enemy"))
+        {
+            Timer += Time.deltaTime;
+            if (Timer >= 1f)
+            {
+                Timer = 0f;
+                other.GetComponent<EnemyBase>().TakeDamage(1);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Move()
     {
         var step = speed * Time.deltaTime;
 
         Vector3 right = new Vector3(100, 0, 0);
 
-        transform.position = Vector3.MoveTowards(transform.position, right , step);
+        transform.position = Vector3.MoveTowards(transform.position, right, step);
 
-        if (transform.position.x > 30 || transform.position.y < -6) 
+        if (transform.position.x > 30 || transform.position.y < -6)
         {
             Destroy(this.gameObject);
         }
+    }
 
+    // Start is called before the first frame update
+    void Start()
+    {
+        // healthBar.UpdateHealthBar(health, maxHealth);
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (! this.Colliding)
+        {
+            this.Move();
+        }
     }
 }
